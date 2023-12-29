a7482616ae1ad1149e48ddf34b552fa3d09c650b
Stephen Baron Bamwite <stephen.bamwite@external.engie.com>
2023-12-29 16:46:02 +0300

    console.log featurecollection with ndvi property

--- extract_data.js
+++ extract_data.js
@@ -11,36 +11,29 @@ var ndvi_image_collection = ee.ImageCollection('LANDSAT/LC08/C02/T1_TOA')
 
 var addNDVI = function(image) {
   var ndvi = image.normalizedDifference(['B5', 'B4']).rename('NDVI');
-  
-}
+  return image.addBands(ndvi);
+};
+
 
 var collectionWithNDVI = ndvi_image_collection.map(addNDVI);
 
-var addNDVIimage = function(image) {
-  // var ndvi = image.normalizedDifference(['B5', 'B4']).rename('NDVI');
-  
+var createNDVITable = function(image) {
   var ndviValue = image.select('NDVI').reduceRegion({
     reducer: ee.Reducer.first(),
-    geometry: roi,
+    geometry: image.geometry().centroid(),
     scale: 30
   }).get('NDVI');
   
   return ee.Feature(null, {
     'system:time_start': image.get('system:time_start'),
-    'NDVI': ndvi
+    'NDVI': ndviValue
   });
 };
 
-var collectionWithNDVI = ndvi_image_collection.map(addNDVIimage);
+var collectionWithNDVITable = collectionWithNDVI.map(createNDVITable);
 
-// // var ndviTable = collectionWithNDVI.select(['NDVI'])
-// //   .reduceColumns({
-// //     reducer: ee.Reducer.toList(),
-// //     selectors: ['system:time_start']
-// //   });
 
-console.log(collectionWithNDVI)
-
+console.log(collectionWithNDVITable)
 
 
 
@@ -51,26 +44,3 @@ console.log(collectionWithNDVI)
 //     fileNamePrefix: 'ndvi_time_series_multiple',
 //     fileFormat: 'CSV'
 // })
-
-// BELOW IS THE EXAMPLE
-// var point = ee.Geometry.Point([-122.292, 37.9018]);
-
-// // Import the Landsat 8 TOA image collection.
-// var l8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_TOA');
-
-// // Get the least cloudy image in 2015.
-// var image = ee.Image(
-//   l8.filterBounds(point)
-//     .filterDate('2015-01-01', '2015-12-31')
-//     .sort('CLOUD_COVER')
-//     .first()
-// );
-
-// var nir = image.select('B5');
-// var red = image.select('B4');
-// var ndvi = nir.subtract(red).divide(nir.add(red)).rename('NDVI');
-
-// // Display the result.
-// Map.centerObject(image, 9);
-// var ndviParams = {min: -1, max: 1, palette: ['blue', 'white', 'green']};
-// Map.addLayer(ndvi, ndviParams, 'NDVI image');
